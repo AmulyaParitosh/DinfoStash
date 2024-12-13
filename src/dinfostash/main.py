76b10b6279dcc4a1cafb0a953e6bc492abd11201
Amulya Paritosh <amulya1171@gmail.com>
2024-12-13 21:00:39 +0530

    FEAT: added fastapi-app title,summary and favicon

--- src/dinfostash/main.py
+++ src/dinfostash/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 
 from dinfostash.auth.routes import router as auth_router
@@ -6,7 +7,10 @@ from dinfostash.data.router import router as data_router
 from dinfostash.resume.router import router as resume_router
 from dinfostash.user.router import router as user_router
 
-app = FastAPI()
+app = FastAPI(
+    title="DinfoStash",
+    summary="A Developer Information Manager that can store and manage developer's portfolio data and also generate resumes.",
+)
 app.include_router(auth_router)
 app.include_router(data_router)
 app.include_router(resume_router)
@@ -18,3 +22,10 @@ app.mount(
     StaticFiles(directory="src/dinfostash/static"),
     name="static",
 )
+
+favicon_path = "src/dinfostash/static/logo.ico"
+
+
+@app.get("/favicon.ico", include_in_schema=False)
+async def favicon() -> FileResponse:
+    return FileResponse(favicon_path)
